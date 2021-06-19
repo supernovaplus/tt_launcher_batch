f38c9f17c35a2c79943ee01bafed028008b393ba
Nova+ <cszonalt@gmail.com>
2021-06-19 20:28:33 +0300

    style changes

--- build.js
+++ build.js
@@ -7,37 +7,35 @@ try{
     fs.mkdirSync(build_folder);
 }catch(err){};
 
-fs.readdirSync(build_folder).map(file => fs.unlinkSync(build_folder + file));
+fs.readdirSync(build_folder).forEach(file => fs.unlinkSync(build_folder + file));
 
-const launcher_data = fs.readFileSync("src/tt_launcher.bat", "utf-8");
-if(!launcher_data) throw new Error("no launcher data");
+const launcher_text = fs.readFileSync("src/tt_launcher.bat", "utf-8");
+if(!launcher_text) throw new Error("no launcher data found #1");
 
-let launcher_version_string = launcher_data.match(/set launcher_version=(v[0-9]+)/g)[0];
-if(!launcher_version_string) throw new Error("no launcher version found");
+let launcher_version_string = launcher_text.match(/set launcher_version=(v[0-9]+)/g)[0];
+if(!launcher_version_string) throw new Error("no launcher version found #2");
 launcher_version_string = launcher_version_string.split("=")[1];
 
-let template = fs.readFileSync(src_folder + "front.html", "utf-8");
-if(!template || typeof template !== "string") throw new Error("Invalid string");
-template = template
+let html = fs.readFileSync(src_folder + "front.html", "utf-8");
+if(!html || typeof html !== "string") throw new Error("no launcher version found #3");
+html = html
 .replace(/%%title%%/g, `TT Launcher ${launcher_version_string}`)
-.replace(/%%bat_code%%/g, launcher_data);
+.replace(/%%bat_code%%/g, launcher_text);
 
 const servers_list_regex1 = /set server_name\[[0-9]+\]=(.*)\r\nset server_endpoint\[[0-9]+\]=(.*)/g;
 const servers_list_regex2 = /set server_name\[[0-9]+\]=(.*)\r\nset server_endpoint\[[0-9]+\]=(.*)/;
 
-let servers_list_match = (Array.from(template.match(servers_list_regex1)) || []);
-if(!servers_list_match) throw new Error("no servers match #1");
+let servers_list_match = (Array.from(html.match(servers_list_regex1)) || []);
+if(!servers_list_match) throw new Error("no servers matched #1");
 
 servers_list_match = (servers_list_match.map(el => servers_list_regex2.exec(el)) || []).map(el => el[1])
-if(!servers_list_match) throw new Error("no servers match #2");
+if(!servers_list_match) throw new Error("no servers matched #2");
 
+html = html
+.replace(/%%servers_list%%/g, servers_list_match.map((server, index) => `${index + 1} - ${server}`).join("<br>\n") + "<br>");
 
-template = template
-.replace(/%%servers_list%%/g, servers_list_match.map((server, index) => `${index + 1} - ${server}`).join("<br>") + "<br>");
-
-fs.writeFileSync(build_folder + "tt_launcher.bat", launcher_data);
-
-fs.writeFileSync(build_folder + "index.html", template);
+fs.writeFileSync(build_folder + "tt_launcher.bat", launcher_text);
+fs.writeFileSync(build_folder + "index.html", html);
 
 // fs.copyFileSync("src/style.css", build_folder + "style.css");
 // fs.copyFileSync("src/code.jpg", build_folder + "code.jpg");
